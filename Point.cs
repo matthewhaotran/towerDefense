dc3f369d38f079a8cd717f6a7ce96f4f7e2d814b
Matthew Tran <matthew@Matthews-MacBook-Pro.local>
2019-03-08 16:14:11 -0800

    add Point.css

---
+++ Point.cs
@@ -0,0 +1,13 @@
+namespace TreehouseDefense
+{
+    class Point
+    {
+        public readonly int X;
+        public readonly int Y;
+        public Map (int x, int y)
+        {
+            X = X;
+            Y = Y;
+        }
+    }
+}
