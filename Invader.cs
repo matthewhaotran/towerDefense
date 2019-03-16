4cc02184bcd1ecbbc82547fa2990dca9b1d24d7e
Matthew Tran <matthew@Matthews-MacBook-Pro.local>
2019-03-16 14:16:35 -0700

    setter and getting in invader class

--- Invader.cs
+++ Invader.cs
@@ -2,6 +2,16 @@ namespace TreehouseDefense
 {
     class Invader
     {
-        
+        private MapLocation _location;
+
+        public MapLocation GetLocation()
+        {
+            return _location;
+        }
+
+        public void SetLocation(MapLocation value)
+        {
+            _location = value;
+        }
     }
 }
