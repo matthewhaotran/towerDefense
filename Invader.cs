ace95a23540eedefc06f45fae95fdd21e15714f3
Matthew Tran <matthew@Matthews-MacBook-Pro.local>
2019-03-16 16:10:49 -0700

    finish invader class

--- Invader.cs
+++ Invader.cs
@@ -4,22 +4,27 @@ namespace TreehouseDefense
     {
         private readonly Path _path;
         private int _pathStep = 0;
-        public MapLocation Location
-        {
-            get
-            {
-                return _path.GetLocationAt(_pathStep);
-            }
-        }
+
+        public MapLocation Location => _path.GetLocationAt(_pathStep);
+
+        public int Health { get; set; } = 2;
+
+        public bool HasScored { get { return _pathStep >= _path.Length; } }
+
+        public bool isNeutralized => Health <= 0;
+
+        public bool isActive => !(isNeutralized || HasScored);
 
         public Invader(Path path)
         {
             _path = path;
         }
 
-        public void Move()
+        public void Move() => _pathStep += 1;
+
+        public void DecreateHealth(int factor)
         {
-            _pathStep += 1;
+            Health -= factor;
         }
     }
 }
