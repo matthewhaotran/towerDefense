e7202b3063be887bb25971873193dd5b63054e7a
Matthew Tran <matthew@Matthews-MacBook-Pro.local>
2019-03-16 11:18:40 -0700

    added exception sheet

--- Game.cs
+++ Game.cs
@@ -8,12 +8,21 @@ namespace TreehouseDefense
         {
             Map map = new Map(8, 5);
 
-            try{
+            try
+            {
             MapLocation mapLocation = new MapLocation(20, 20, map);
+            }
+            catch(OutOfBoundsException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(TreehouseDefenseException)
+            {
+                Console.WriteLine("Unhandled TreehouseDefenseException");
             }
             catch(Exception)
             {
-                Console.WriteLine("That map location is not on the map!");
+                Console.WriteLine("Unhandled Exception");
             }
         }
     }
