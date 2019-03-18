885f07c3b9ae63d1256797ea4d2c6dc2ad0ecbbc
Matthew Tran <matthew@Matthews-MacBook-Pro.local>
2019-03-18 14:45:29 -0700

    Add FireOnInvaders while look in Tower class

--- Tower.cs
+++ Tower.cs
@@ -2,6 +2,23 @@ namespace TreehouseDefense
 {
     class Tower
     {
-        
+        private readonly MapLocation _location;
+
+        public Tower(MapLocation location)
+        {
+            _location = location;
+        }
+
+        public void FireOnInvaders(Invader[] invaders)
+        {
+            int index = 0;
+            while(index < invaders.Length)
+            {
+                Invader invader = invaders[index];
+                //Do stuff with invdader
+
+                index++;
+            }
+        }
     }
 }
